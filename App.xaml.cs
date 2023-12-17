131987f0ec8ce1f572a9cad8d6f873cabb708aef
SerbanAruncutean <serban.aruncutean@yahoo.ro>
2023-12-17 18:38:34 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,25 @@
-﻿namespace Aruncutean_Serban_7;
+﻿using System;
+using Aruncutean_Serban_7.Data;
+using System.IO;
+namespace Aruncutean_Serban_7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
